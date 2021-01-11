3a4ca5047e31eb3f6b89b0c9f5d2642dc0ba5317
vk <valeriia.koltsova@gmail.com>
2021-01-11 18:31:45 -0500

    add empty SplashScreenActivity.kt to implement second activity

---
+++ app/src/main/java/com/onramp/android/takehome/SplashScreenActivity.kt
@@ -0,0 +1,21 @@
+package com.onramp.android.takehome
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        lifecycleScope.launch {
+            delay(2000)
+            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
